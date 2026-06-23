9f21b86095b4116cd8b89e0df95e4535248a18ab
FranciscoDastres <Francisco.meza.dastres@gmail.com>
2026-06-23 16:56:54 -0400

    feat(auth): add identity domain errors

--- apps/backend/src/domain/errors/domain.error.ts
+++ apps/backend/src/domain/errors/domain.error.ts
@@ -11,6 +11,14 @@ export class AppointmentInPastError extends DomainError {}
 export class AppointmentOutsideBusinessHoursError extends DomainError {}
 export class EmailAlreadyRegisteredError extends DomainError {}
 export class InvalidCredentialsError extends DomainError {}
+export class AccountSuspendedError extends DomainError {}
+export class EmailNotVerifiedError extends DomainError {}
+export class InvalidActionTokenError extends DomainError {}
+export class InvalidGoogleTokenError extends DomainError {}
+export class GoogleUnavailableError extends DomainError {}
+export class GoogleLinkRequiredError extends DomainError {}
+export class LastLoginMethodError extends DomainError {}
+export class InvalidRefreshTokenError extends DomainError {}
 export class UserNotFoundError extends DomainError {}
 export class CannotChangeOwnRoleError extends DomainError {}
 export class PetNotFoundError extends DomainError {}
